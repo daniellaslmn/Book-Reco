737fe3db7b51ea8fc0b933ee3eefd9b1e5902666
daniellaslmn <daniellalyn.soliman@gmail.com>
2025-12-27 00:55:47 +0800

    Initial backend and mobile setup

---
+++ mobile/app/index.jsx
@@ -0,0 +1,26 @@
+import { Link } from "expo-router";
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}>
+      <Text style={styles.title}>Choi Seungcheol with Yoon Jeonghan</Text>
+     
+      <Link href="/(auth)/signup">Signup</Link>
+      <Link href="/(auth)">Login Page</Link>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color:"black",
+  }
+});
